0801d21b28aec1c97c82684aa170c717729f2827
Shreyaans Nahata <shreyaansnahata2001@gmail.com>
2021-07-12 17:38:02 +0530

    Add files via upload

---
+++ src/com/telegraph/Launcher.java
@@ -0,0 +1,22 @@
+package com.telegraph;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("view/config.fxml"));
+
+        Scene scene = new Scene(parent, 300, 250);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
