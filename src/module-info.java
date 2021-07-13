97d754962712e609c896e5d7a4b02c27ee289afc
Shreyaans Nahata <shreyaansnahata2001@gmail.com>
2021-07-13 23:24:50 +0530

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Telegraph {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.web;
-
-    opens com.telegraph;
-    opens com.telegraph.view;
-}
